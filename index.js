9f6ee273653176c7116cf1d7a447a5d1b90c493b
ivy.chen <sh22789ml@SH22789MLdeMacBook-Pro.local>
2021-12-09 18:58:20 +0800

    feat: add create file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const inquirer = require('inquirer');
 const program = require('commander');
 const chalk = require('chalk'); // 注意此处版本问题，5.x.x 版本的不支持 require 引入，只能 import @todo 可研究如何使用最新版本
+const figlet = require('figlet');
 
 // 2.chalk 命令行美化 
 
@@ -28,12 +29,53 @@ program
   .version(`v${require('./package.json').version}`)
   .usage('<command> [option]')
 
+// 5. 配置 config 命令 @todo 可以学习一下 vue-cli 里面的一些命令以及实现
+program
+  .command('config [value]')
+  .description('inspect and modify the config')
+  .option('-g, --get <path>', 'get value from option')
+  .option('-s, --set <path> <value>')
+  .option('-d, --delete <path>', 'delete option from config')
+  .action((value, options) => {
+    console.log(value, options)
+  })
+
+// 配置 ui 命令
+program
+  .command('ui')
+  .description('start add open roc-cli ui')
+  .option('-p, --port <port>', 'Port used for the UI Server')
+  .action((option) => {
+    console.log(option)
+  })
+
+// 7. 完善帮助信息
+program
+  // 监听 --help 执行
+  .on('--help', () => {
+
+    // 7.1 使用 figlet 绘制 Logo
+    // 先 yarn add figlet 一下
+    console.log('\r\n' + figlet.textSync('skeleton', {
+      font: 'Ghost',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      width: 120,
+      whitespaceBreak: true
+    }));
+
+    // 新增说明信息
+    console.log(`\r\nRun ${chalk.cyan(`skeleton-cli <command> --help`)} for detailed usage of given command\r\n`)
+  })
+
+
 // program.parse()
 // 4.2 解析用户执行命令传入参数
 program.parse(process.argv)
 
 
 // 3.询问 命令行交互工具
+// 8.询问用户问题 并 获取创建所需要的信息
 // inquirer.prompt([
 //   {
 //     type: 'input', // type： input, number, confirm, list, checkbox ... 
--- libs/create.js
+++ libs/create.js
@@ -1,4 +1,32 @@
+const path = require('path')
+const fs = require('fs-extra')
+
+// 询问之后，创建目录
+// 需要思考：是否已经存在？是否需要强制覆盖？
+// 如果存在
+// 当 { force: true } 时，直接移除原来的目录，直接创建
+// 当 { force: false } 时 询问用户是否需要覆盖
+// 如果不存在，直接创建
+
 module.exports = async function (name, options) {
   // 验证是否正常取到值
   console.log('>>> create.js', name, options) // 打印出来 appdemo { force: true }
+
+  // 执行创建命令
+
+  // 当前命令行选择的目录
+  const cwd  = process.cwd();
+  // 需要创建的目录地址
+  const targetAir  = path.join(cwd, name)
+
+  // 目录是否已经存在？
+  if (fs.existsSync(targetAir)) {
+
+    // 是否为强制创建？
+    if (options.force) {
+      await fs.remove(targetAir)
+    } else {
+      // TODO：询问用户是否确定要覆盖
+    }
+  }
 }
